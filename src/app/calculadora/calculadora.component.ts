eeaab938d49694d8f4981bb0fb398e2b350520ba
Ricardo J. Castillo López <87798579+rjcastillolopez@users.noreply.github.com>
2023-01-04 15:47:22 -0500

    updates

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -6,33 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
-  private numeroActual: string = '';
-  private numeroAnterior: string = '';
-  private operador: string = '';
+  private numeroActual: string | null = null
+  private numeroAnterior: string | null = null
+  private operador: string | null = null
   private operaciones: string[] = ['+', '-', '*', '/'];
+  fecha: string = '';
   salida: string = '';
-
 
   agregar(valor: string): void {
     if (this.operaciones.includes(valor)) {
       this.operador = valor;
       this.numeroAnterior = this.numeroActual;
       this.numeroActual = '';
-      this.salida = this.salida + valor;
+      this.salida += valor;
     } 
     else if (valor === '=') {
-      this.calcular();
+      if (this.numeroActual === null) {
+        this.numeroActual = this.numeroAnterior;
+      } else {
+        this.calcular();
+      }
     } 
     else if (this.salida === '') {
       this.salida = "S/" + valor;
-      this.numeroActual += valor;
+      if (this.numeroActual) {
+        this.numeroActual += valor;
+      } else {
+        this.numeroActual = valor;
+      }
     }
     else if (this.salida[this.salida.length-1] === this.operador) {
-      this.salida = this.salida + "S/" + valor;
+      this.salida += "S/" + valor;
       this.numeroActual += valor;
     }
     else {
-      this.salida = this.salida + valor;
+      this.salida += valor;
       this.numeroActual += valor;
     }
     console.log(this.numeroActual, this.numeroAnterior, this.operador, this.salida)
@@ -53,14 +61,27 @@ export class CalculadoraComponent {
         this.numeroActual = (Number(this.numeroAnterior) / Number(this.numeroActual)).toString();
         break;
     }
+    this.numeroAnterior = null;
+    this.operador = null;
     this.salida = "S/" + this.numeroActual;
-    console.log(this.numeroAnterior, this.numeroActual);
   }
 
   limpiar(): void {
     this.salida = '';
-    this.numeroActual = '';
-    this.numeroAnterior = '';
-    this.operador = '';
+    this.numeroActual = null;
+    this.numeroAnterior = null;
+    this.operador = null;
+  }
+
+  borrar(): void {
+    this.salida = this.salida.slice(0, -1);
+    if (this.numeroActual !== null) {
+      this.numeroActual = this.numeroActual.slice(0, -1);
+    }
+  }
+
+  getDate(): void {
+    let date = new Date();
+    this.fecha = date.toLocaleDateString();
   }
 }
